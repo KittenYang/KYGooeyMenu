cc4051dafa406d26d3735bb55137e9fcc6297c3f
KittenYang <kittenyang@icloud.com>
2015-08-25 02:59:04 +0800

    v2 going...

--- Classes/MenuLayer/MenuLayer.m
+++ Classes/MenuLayer/MenuLayer.m
@@ -77,9 +77,9 @@
         }
         moveDistance_1 = (real_rect.size.width/2 - offset)/2;
         moveDistance_2 = _xAxisPercent*(real_rect.size.width/3);
-        top_left   =  CGPointMake(center.x-offset-moveDistance_1*2 + moveDistance_2, OFF-20*hightFactor);
-        top_center =  CGPointMake(center.x-moveDistance_1 + moveDistance_2, OFF-25*hightFactor);
-        top_right  =  CGPointMake(center.x+offset+moveDistance_2, OFF-20*hightFactor);
+        top_left   =  CGPointMake(center.x-offset-moveDistance_1*2 + moveDistance_2, OFF-5*hightFactor);
+        top_center =  CGPointMake(center.x-moveDistance_1 + moveDistance_2, OFF-10*hightFactor);
+        top_right  =  CGPointMake(center.x+offset+moveDistance_2, OFF);
         
     }else if(_animState == STATE3){
         
